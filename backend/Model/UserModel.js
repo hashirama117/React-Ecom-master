49662e37450412b97dac7a7fece8debb26fcab7d
haseeb <arsalmughal4@gmail.com>
2023-04-04 11:23:51 +0500

    update-in-new-pc

--- backend/Model/UserModel.js
+++ backend/Model/UserModel.js
@@ -1,20 +1,23 @@
-const mongoose=require('mongoose');
-const userSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'please add a name'],
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add a name"],
     },
-    email:{
-        type:String,
-        required:[true,'please add an email'],
-        unique:true,
+    email: {
+      type: String,
+      required: [true, "please add an email"],
+      unique: true,
     },
-    password:{
-        type:String,
-        required:[true,'please add an email'],
-        unique:true,
+    password: {
+      type: String,
+      required: [true, "please add a password"],
+      unique: true,
     },
-},{
-    timestamps:true,
-})
-module.exports=mongoose.model('Users',userSchema);
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Users", userSchema);
